a90608be0890f512b3d2cc8b93598b2e3f511daf
kevinrodbe <kevinrodbe@users.noreply.github.com>
2025-07-21 00:02:11 -0500

    feat: export src files + convert ts to js

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,12 @@
-// @ts-check
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
+import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
 import json from '@eslint/json';
-import tsEslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier/recommended';
-import packageJson from 'eslint-plugin-package-json/configs/recommended';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// TODO: check type error
+// import packageJson from 'eslint-plugin-package-json/configs/recommended';
 
 // eslint-disable-next-line no-restricted-exports
-export default tsEslint.config(
+export default defineConfig([
   { ignores: ['**/dist/', '**/docs/', '**/coverage/', '**/*.d.ts', 'node_modules'] },
   {
     files: ['**/*.json'],
@@ -42,60 +37,9 @@ export default tsEslint.config(
     },
   },
   {
-    extends: [
-      js.configs.recommended,
-      tsEslint.configs.strictTypeChecked,
-      tsEslint.configs.stylisticTypeChecked,
-    ],
-    files: ['**/*.js', '**/*.ts'],
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ['eslint.config.js'],
-        },
-        tsconfigRootDir: __dirname,
-      },
-    },
+    extends: [js.configs.recommended],
+    files: ['**/*.js'],
     rules: {
-      '@typescript-eslint/array-type': [
-        'error',
-        {
-          default: 'generic',
-        },
-      ],
-      '@typescript-eslint/consistent-type-exports': 'error',
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        {
-          fixStyle: 'inline-type-imports',
-          prefer: 'type-imports',
-        },
-      ],
-      '@typescript-eslint/member-ordering': [
-        'error',
-        {
-          default: {
-            memberTypes: ['method', 'field'],
-            order: 'alphabetically-case-insensitive',
-          },
-        },
-      ],
-      '@typescript-eslint/no-explicit-any': [
-        'error',
-        {
-          fixToUnknown: true,
-        },
-      ],
-      '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
-      '@typescript-eslint/no-unnecessary-template-expression': 'error',
-      '@typescript-eslint/no-unnecessary-type-arguments': 'error',
-      '@typescript-eslint/no-unnecessary-type-constraint': 'error',
-      '@typescript-eslint/prefer-as-const': 'error',
-      '@typescript-eslint/prefer-includes': 'error',
-      '@typescript-eslint/prefer-optional-chain': 'error',
-      '@typescript-eslint/prefer-reduce-type-parameter': 'error',
-      '@typescript-eslint/prefer-regexp-exec': 'error',
-      '@typescript-eslint/promise-function-async': 'error',
       curly: 'error',
       'max-params': ['error', 2],
       'newline-after-var': 'error',
@@ -132,6 +76,6 @@ export default tsEslint.config(
       'spaced-comment': 'error',
     },
   },
-  packageJson,
-  prettier
-);
+  // packageJson,
+  prettier,
+]);
